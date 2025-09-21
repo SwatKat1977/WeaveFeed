df9bfcf6d9fbb8a958ac74360e1c82b3f1f549dc
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-09-21 19:02:05 +0100

    custom route decorators

---
+++ common/weavefeed_common/route_decorators.py
@@ -0,0 +1,12 @@
+"""
+Copyright (C) 2025  WeaveFeed Development Team
+SPDX-License-Identifier: AGPL-3.0-or-later
+
+This file is part of WeaveFeed. See the LICENSE file in the project
+root for full license details.
+"""
+
+
+def route_not_using_db(func):
+    func._not_using_db = True
+    return func
