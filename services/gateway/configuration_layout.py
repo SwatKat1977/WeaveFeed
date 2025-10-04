332c1b405c21b40926f4e7b340ea96d68a9cf603
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-10-04 09:40:10 +0100

    framework for gateway service

--- services/gateway/configuration_layout.py
+++ services/gateway/configuration_layout.py
@@ -5,3 +5,14 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 This file is part of WeaveFeed. See the LICENSE file in the project
 root for full license details.
 """
+from weavefeed_common.configuration import configuration_setup
+
+CONFIGURATION_LAYOUT = configuration_setup.ConfigurationSetup(
+    {
+        "logging": [
+            configuration_setup.ConfigurationSetupItem(
+                "log_level", configuration_setup.ConfigItemDataType.STRING,
+                valid_values=['DEBUG', 'INFO'], default_value="INFO")
+        ]
+    }
+)
