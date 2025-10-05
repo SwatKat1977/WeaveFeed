ea1616c9a34e97749170680bedda89000a6b589f
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-10-05 23:40:18 +0100

    work on health endpoint

--- services/accounts/api/health_api.py
+++ services/accounts/api/health_api.py
@@ -5,3 +5,40 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 This file is part of WeaveFeed. See the LICENSE file in the project
 root for full license details.
 """
+import logging
+from quart import Blueprint
+from api.health_api_view import HealthApiView
+from state_object import StateObject
+
+
+def create_blueprint(logger: logging.Logger,
+                     state_object: StateObject) -> Blueprint:
+    """
+    Creates and returns a Quart Blueprint for the Health Status API.
+
+    This function initializes a `HealthApiView` instance and registers an
+    asynchronous route `/health/status` for handling health check requests. The
+    route is logged upon registration.
+
+    Args:
+        logger (logging.Logger): The logger instance used for logging API
+                                 registration.
+        state_object (StateObject): The application state object passed to the
+                                    view.
+
+    Returns:
+        Blueprint: A Quart Blueprint instance with the registered health status
+                   route.
+    """
+    view = HealthApiView(logger, state_object)
+
+    blueprint = Blueprint('health_api', __name__)
+
+    logger.debug("Registering Health Status API:")
+    logger.debug("=> /health [GET]")
+
+    @blueprint.route('/health', methods=['GET'])
+    async def authenticate_request():
+        return await view.health()
+
+    return blueprint
